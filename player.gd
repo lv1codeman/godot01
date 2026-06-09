c852247a386057eccb54cb06a7390f0400432dcf
lv1codeman <lv1codeman@gmail.com>
2026-06-09 15:43:35 +0800

    05 done

---
+++ player.gd
@@ -0,0 +1,134 @@
+extends CharacterBody2D
+
+enum State {
+	IDLE,
+	RUNNING,
+	JUMP,
+	FALL,
+	LANDING,
+}
+
+const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
+const RUN_SPEED := 160.0
+const FLOOR_ACCELERATION := RUN_SPEED / 0.2
+const AIR_ACCELERATION := RUN_SPEED / 0.2
+const JUMP_VELOCITY := -320.0
+
+var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var is_first_tick := false
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var jump_request_timer: Timer = $JumpRequestTimer
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		jump_request_timer.start()
+	if event.is_action_released("jump"):
+		jump_request_timer.stop()
+		if velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+
+func tick_physics(state: State, delta: float) -> void:
+	match state:
+		State.IDLE:
+			move(default_gravity, delta)
+		State.RUNNING:
+			move(default_gravity, delta)
+		State.JUMP:
+			move(0.0 if is_first_tick else default_gravity, delta)
+		State.FALL:
+			move(default_gravity, delta)
+		State.LANDING:
+			# 檢查是否有左右輸入
+			var direction := Input.get_axis("move_left", "move_right")
+			if not is_zero_approx(direction):
+				move(default_gravity, delta) # 有輸入就移動
+			else:
+				stand(delta) # 沒輸入就原地滑行煞車
+	is_first_tick = false
+
+func move(gravity: float, delta: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right")
+	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+	velocity.x = move_toward(velocity.x, direction * RUN_SPEED, acceleration * delta)
+	velocity.y += gravity * delta
+	
+	if not is_zero_approx(direction):
+		sprite_2d.flip_h = direction < 0
+	
+	move_and_slide()
+
+func stand(delta: float) -> void:
+	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+	velocity.x = move_toward(velocity.x, 0.0, acceleration * delta)
+	velocity.y += default_gravity * delta
+	move_and_slide()
+
+func get_next_state(state: State) -> State:
+	var can_jump := is_on_floor() or coyote_timer.time_left > 0
+	var shouldjump := can_jump and jump_request_timer.time_left > 0
+	if shouldjump:
+		return State.JUMP
+	var direction := Input.get_axis("move_left", "move_right")
+	var has_input := not is_zero_approx(direction)
+	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
+	
+	match state:
+		State.IDLE:
+			if not is_on_floor():
+				return State.FALL
+			if not is_still:
+				return State.RUNNING
+		State.RUNNING:
+			if not is_on_floor():
+				return State.FALL
+			if is_still:
+				return State.IDLE
+		State.JUMP:
+			if velocity.y >= 0:
+				return State.FALL
+		State.FALL:
+			if is_on_floor():
+				return State.LANDING if is_still else State.RUNNING
+		State.LANDING:
+			if not is_on_floor():
+				return State.FALL
+			if has_input: 
+				return State.RUNNING
+			if not animation_player.is_playing():
+				return State.IDLE
+			
+	return state
+
+
+func transition_state(from: State, to: State) -> void:
+	if from not in GROUND_STATES and to in GROUND_STATES:
+		coyote_timer.stop()
+	
+	match to:
+		State.IDLE:
+			animation_player.play("idle")
+	
+		State.RUNNING:
+			animation_player.play("running")
+			
+		State.JUMP:
+			animation_player.play("jump")
+			velocity.y = JUMP_VELOCITY
+			coyote_timer.stop()
+			jump_request_timer.stop()
+	
+		State.FALL:
+			animation_player.play("fall")
+			if from in GROUND_STATES:
+				coyote_timer.start()
+	
+		State.LANDING:
+			animation_player.play("landing")
+	
+	is_first_tick = true
+	
+	
+	
