fc305348ea52b4131d0f80ac1b87814bace53d40
lv1codeman <lv1codeman@gmail.com>
2026-06-11 09:34:46 +0800

    06 before coding

--- player.gd
+++ player.gd
@@ -16,7 +16,8 @@ const JUMP_VELOCITY := -320.0
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false
 
-@onready var sprite_2d: Sprite2D = $Sprite2D
+
+@onready var graphics: Node2D = $Graphics
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var jump_request_timer: Timer = $JumpRequestTimer
@@ -50,7 +51,7 @@ func move(gravity: float, delta: float) -> void:
 	velocity.y += gravity * delta
 	
 	if not is_zero_approx(direction):
-		sprite_2d.flip_h = direction < 0
+		graphics.scale.x = -1 if direction < 0 else +1
 	
 	move_and_slide()
 
