980f6cee48b91c1c0db233102aa97983e1955477
lv1codeman <lv1codeman@gmail.com>
2026-06-11 15:48:53 +0800

    07 done

--- player.gd
+++ player.gd
@@ -6,13 +6,15 @@ enum State {
 	FALL,
 	LANDING,
 	WALL_SLIDING,
+	WALL_JUMP,
 }
 
 const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
 const RUN_SPEED := 160.0
 const FLOOR_ACCELERATION := RUN_SPEED / 0.2
-const AIR_ACCELERATION := RUN_SPEED / 0.2
+const AIR_ACCELERATION := RUN_SPEED / 0.1
 const JUMP_VELOCITY := -320.0
+const WALL_JUMP_VELOCITY := Vector2(300, -300)
 
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false
@@ -24,6 +26,9 @@ var is_first_tick := false
 @onready var jump_request_timer: Timer = $JumpRequestTimer
 @onready var hand_checker: RayCast2D = $Graphics/HandChecker
 @onready var foot_checker: RayCast2D = $Graphics/FootChecker
+@onready var state_machine: StateMachine = $StateMachine
+
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
@@ -44,10 +49,17 @@ func tick_physics(state: State, delta: float) -> void:
 		State.FALL:
 			move(default_gravity, delta)
 		State.LANDING:
-			stand(delta) # 沒輸入就原地滑行煞車
+			stand(default_gravity, delta) # 沒輸入就原地滑行煞車
 		State.WALL_SLIDING:
 			move(default_gravity / 3, delta)
 			graphics.scale.x = get_wall_normal().x
+		State.WALL_JUMP:
+			if state_machine.state_time < 0.1:
+				stand(0.0 if is_first_tick else default_gravity, delta)
+				graphics.scale.x = get_wall_normal().x
+			else:
+				move(default_gravity, delta)
+			
 	is_first_tick = false
 
 func move(gravity: float, delta: float) -> void:
@@ -61,11 +73,14 @@ func move(gravity: float, delta: float) -> void:
 	
 	move_and_slide()
 
-func stand(delta: float) -> void:
+func stand(gravity: float, delta: float) -> void:
 	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
 	velocity.x = move_toward(velocity.x, 0.0, acceleration * delta)
 	velocity.y += default_gravity * delta
 	move_and_slide()
+
+func can_wall_slide() -> bool:
+	return is_on_wall() and hand_checker.is_colliding() and foot_checker.is_colliding()
 
 func get_next_state(state: State) -> State:
 	var can_jump := is_on_floor() or coyote_timer.time_left > 0
@@ -92,7 +107,7 @@ func get_next_state(state: State) -> State:
 		State.FALL:
 			if is_on_floor():
 				return State.LANDING if is_still else State.RUNNING
-			if is_on_wall() and hand_checker.is_colliding() and foot_checker.is_colliding():
+			if can_wall_slide():
 				return State.WALL_SLIDING
 		State.LANDING:
 			if not is_still:
@@ -100,15 +115,28 @@ func get_next_state(state: State) -> State:
 			if not animation_player.is_playing():
 				return State.IDLE
 		State.WALL_SLIDING:
+			if jump_request_timer.time_left > 0:
+				return State.WALL_JUMP
 			if is_on_floor():
 				return State.IDLE
 			if not is_on_wall():
 				return State.FALL
 			
+		State.WALL_JUMP:
+			if can_wall_slide() and not is_first_tick:
+				return State.WALL_SLIDING
+			if velocity.y >= 0:
+				return State.FALL
+		
 	return state
 
 
 func transition_state(from: State, to: State) -> void:
+	print("[%s] %s => %s" %[
+		Engine.get_physics_frames(),
+		State.keys()[from] if from != -1 else "<START>",
+		State.keys()[to],
+	])
 	if from not in GROUND_STATES and to in GROUND_STATES:
 		coyote_timer.stop()
 	
@@ -135,6 +163,18 @@ func transition_state(from: State, to: State) -> void:
 	
 		State.WALL_SLIDING:
 			animation_player.play("wall_sliding")
+			
+		State.WALL_JUMP:
+			animation_player.play("jump")
+			velocity = WALL_JUMP_VELOCITY
+			velocity.x *= get_wall_normal().x
+			jump_request_timer.stop()
+			
+	#if to == State.WALL_JUMP:
+		#Engine.time_scale = 0.3 # 進入WALL_JUMP狀態時， 遊戲時間流速降低為0.3倍
+	#if from == State.WALL_JUMP:
+		#Engine.time_scale = 1.0
+			
 	is_first_tick = true
 	
 	
